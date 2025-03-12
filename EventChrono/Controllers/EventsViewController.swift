b0869782ba268234c58dd66d507ec105949cb37e
PeterDumanov <dumanovkrd@yandex.ru>
2025-03-12 21:09:50 +0100

    Add event loading functionality

--- EventChrono/Controllers/EventsViewController.swift
+++ EventChrono/Controllers/EventsViewController.swift
@@ -10,11 +10,13 @@ import CoreData
 
 class EventsViewController: UITableViewController {
     
-    var events = ["First", "Second", "Test"]
+    var events = [Event]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadEvents()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -23,15 +25,30 @@ class EventsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
-        cell.textLabel?.text = events[indexPath.row]
+        let event = events[indexPath.row]
+        cell.textLabel?.text = "\(event.name!) \(event.date!)"
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "goToAddEvent", sender: self)
+    }
+    
+    func loadEvents(with request: NSFetchRequest<Event> = Event.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        request.predicate = predicate
+        
+        do {
+            events = try context.fetch(request)
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        tableView.reloadData()
     }
     
 }
