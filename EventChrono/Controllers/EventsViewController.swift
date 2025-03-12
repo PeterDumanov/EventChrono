851efa34d66f83d65bbfe88ea8510a3e2bd045de
PeterDumanov <dumanovkrd@yandex.ru>
2025-03-12 19:09:02 +0100

    Add data model for event

--- EventChrono/Controllers/EventsViewController.swift
+++ EventChrono/Controllers/EventsViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class EventsViewController: UITableViewController {
     
-    var eventsArray = ["First", "Second", "Test"]
+    var events = ["First", "Second", "Test"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +17,12 @@ class EventsViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return eventsArray.count
+        return events.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
-        cell.textLabel?.text = eventsArray[indexPath.row]
+        cell.textLabel?.text = events[indexPath.row]
         return cell
     }
     
