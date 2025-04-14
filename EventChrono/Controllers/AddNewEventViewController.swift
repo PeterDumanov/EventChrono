015186441d1eafc6a0952488cf7a679aa65d33d7
PeterDumanov <dumanovkrd@yandex.ru>
2025-04-14 19:55:53 +0200

    Add minor UI improvements

--- EventChrono/Controllers/AddNewEventViewController.swift
+++ EventChrono/Controllers/AddNewEventViewController.swift
@@ -86,4 +86,8 @@ extension AddNewEventViewController: UITableViewDelegate, UITableViewDataSource 
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
